7f9404e299e05d7ef54597a6d2688fe765894bd3
Jeff <35687904+JeffG22@users.noreply.github.com>
2018-05-18 13:03:17 +0200

    Aggiunta l'uscita con exit secondo la convenzione. Cambiato il do while con un while + assegnamento.

--- parameters.c
+++ parameters.c
@@ -5,22 +5,21 @@
 #include <getopt.h>
 #include <stdlib.h>
 
-const char *program_name;
-
-void print_help(FILE *stream) {
+void print_help(FILE *stream, int EXIT_CODE) {
     fprintf(stream, "Simulator. Options:\n");
     fprintf(stream,
             "  -h   --help                           Display this help message.\n"
             "  -op  --output-preemption filename     Set output file for preemptive scheduler.\n"
             "  -on  --output-no-preemption filename  Set output file for non-preemptive scheduler.\n"
             "  -i   --input                          Set file containing tasks.\n");
-    exit(0);
+    exit(EXIT_CODE);
 }
 
 
 int main(int argc, char* argv[]) {
+
     int next_option;
-    extern char *optarg;
+    //extern char *optarg; pensavo fosse necessario
 
     const char* const short_options = "hi:";
 
@@ -35,9 +34,7 @@ int main(int argc, char* argv[]) {
     };
 
     if (argc != 1)
-        do {
-            next_option = getopt_long_only(argc, argv, short_options, long_options, NULL);
-
+        while ((next_option = getopt_long_only(argc, argv, short_options, long_options, NULL)) != -1) { //verifica condizione con assegnamento
             switch (next_option) {
                 case 'p':
                     printf("OP. One day I'll now what this means: %s\n", optarg);
@@ -49,14 +46,17 @@ int main(int argc, char* argv[]) {
                     printf("ON. One day I'll now what this means: %s\n", optarg);
                     break;
                 case 'h':
-                case '?':
-                    print_help(stdout);
+                    print_help(stdout, EX_OK); //EX_OK equivale a 0
                     break;
-                case -1:
+                case '?': /* getopt_long printed an error message. */
+                    print_help(stderr, EX_USAGE); //EX_USAGE indica errore nell'invocazione da linea di comando
                     break;
                 default:
                     abort();
             }
-        } while (next_option != -1);
+        }
+    else
+        print_help(stderr, EX_USAGE); //il programma è stato avviato senza parametri.
+
     return 0;
 }
